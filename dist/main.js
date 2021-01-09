92f5741f9afc44916a306087f6395f512ce6c5e5
Baris Bilgin <brokoliweb@gmail.com>
2021-01-09 11:04:25 +0300

    fix style issue - add responsiveness

--- dist/main.js
+++ dist/main.js
@@ -110,6 +110,8 @@ function createWelcomeMessage() {
   welcomeMessage.innerHTML =
     "Dear Guests, you are welcomed to dine with us at Our Restaurant. We will serve you with the best dishes. Hope you have a pleasant dining experience with US.";
 
+  welcomeMessage.classList.add('welcome')
+
   document.getElementById("content").appendChild(welcomeMessage);
 }
 
